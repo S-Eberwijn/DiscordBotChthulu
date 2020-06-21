6c6e322ee9497ba6a9bdafa0b28029dbb6570f41
Stephan <11700946@student.pxl.be>
2020-06-21 10:28:18 +0200

    refactored more

--- commands/Dungeons&Dragons/sessionRequest.js
+++ commands/Dungeons&Dragons/sessionRequest.js
@@ -1,18 +1,6 @@
-const { MessageEmbed } = require('discord.js');
 const { writeToJsonDb } = require('../../otherFunctions/writeToJsonDb.js');
-const { decimalToHex, getDoubleDigitNumber } = require("../../otherFunctions/numberFunctions");
-const { getSessionRequestPartyMembers, getSessionRequestObjective } = require("../../otherFunctions/sessionRequestFunctions")
+const { getSessionRequestPartyMembers, getSessionRequestObjective, getSessionRequestDateAndTime, createSessionRequestEmbed } = require("../../otherFunctions/sessionRequestFunctions")
 // use: !session <sessionCommander> ... <n < 5> <dd/mm> <hh:mm> <objective>
-
-const days = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday'
-]
 
 module.exports.run = async (bot, message, args) => {
     let dmRole = message.guild.roles.cache.find(role => role.name === 'Dungeon Master');
@@ -21,47 +9,14 @@ module.exports.run = async (bot, message, args) => {
     let partyMembers = getSessionRequestPartyMembers(args, bot);
 
     // get the right date and time
-    let suppliedDateText = args[0];
-    args.shift();
-
-    let hourAndMinute = args[0];
-    let hour = parseInt(hourAndMinute.substring(0, 2));
-    let minute = parseInt(hourAndMinute.substring(3, 5));
-    args.shift();
-
-    let displayDate = new Date(`${new Date().getFullYear()}-${suppliedDateText.substring(suppliedDateText.length - 2, suppliedDateText.length)}-${suppliedDateText.substring(0, 2)}`)
-    displayDate.setHours(hour);
-    displayDate.setMinutes(minute);
+    let displayDate = getSessionRequestDateAndTime(args)
 
     // get the objective
     let objective = getSessionRequestObjective(args);
 
-
-    let partyMembersText = 'Something went wrong!';
-
     // Check if the one who sent the message has the 'Dungeon Master' role
     if (message.guild.member(message.author).roles.cache.has(dmRole.id)) {
-
-        partyMembersText = `${partyMembers[0]}`
-        for (let i = 1; i < partyMembers.length; i++) {
-            partyMembersText += `, ${partyMembers[i]}`;
-        }
-
-        // output message
-        let outputEmbed = new MessageEmbed()
-            .setColor(`#${decimalToHex(bot.sessions.nextSessionId)}`)
-            .setThumbnail(message.guild.iconURL())
-            .setTitle(`**Session_Request: **`)
-            .addFields(
-                { name: `**Session Commander:**`, value: `${partyMembers[0]}\n`, inline: false },
-                { name: `**Players(${partyMembers.length}/5):**`, value: `${partyMembersText}`, inline: false },
-                { name: `**DM:**`, value: `*TBD*`, inline: false },
-                { name: `**Time:**`, value: `*${days[displayDate.getDay()]} (${getDoubleDigitNumber(displayDate.getDate())}/${getDoubleDigitNumber(displayDate.getMonth() + 1)}) ${getDoubleDigitNumber(displayDate.getHours())}:${getDoubleDigitNumber(displayDate.getMinutes())}*`, inline: false },
-                { name: `**Location:**`, value: `*Roll20 (online)*`, inline: false },
-                { name: `**Objective:**`, value: `*${objective.trim()}*`, inline: false }
-            )
-            .setTimestamp()
-            .setFooter(`Requested by ${message.author.username}`, message.author.avatarURL());
+        let outputEmbed = createSessionRequestEmbed(partyMembers, displayDate, objective, message, bot);
 
         message.delete();
         message.channel.send(outputEmbed);
@@ -79,11 +34,9 @@ module.exports.run = async (bot, message, args) => {
         bot.sessions.nextSessionId += 1;
 
         writeToJsonDb("sessions", bot.sessions);
-
     } else {
         message.channel.send("My masters have told me not to listen to you!");
     }
-
 }
 
 module.exports.help = {
--- otherFunctions/sessionRequestFunctions.js
+++ otherFunctions/sessionRequestFunctions.js
@@ -1,4 +1,16 @@
+const { MessageEmbed } = require('discord.js');
+const { decimalToHex, getDoubleDigitNumber } = require("../otherFunctions/numberFunctions");
 const {getUserFromMention} = require("./getUserFromMention");
+
+const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
 
 exports.getSessionRequestPartyMembers = function (args, bot) {
     let partyMembers = [];
@@ -18,9 +30,49 @@ exports.getSessionRequestObjective = function (args) {
     return objective;
 }
 
-// exports.hexToDec = function (hexString) {
-//     if (hexString.charAt(0) === "#"){
-//         hexString = hexString.slice(1);
-//     }
-//     return parseInt(hexString, 16);
-// }
+exports.getSessionRequestDateAndTime = function (args) {
+    let suppliedDateText = args[0];
+    args.shift();
+
+    let hourAndMinute = args[0];
+    let hour = parseInt(hourAndMinute.substring(0, 2));
+    let minute = parseInt(hourAndMinute.substring(3, 5));
+    args.shift();
+
+    let displayDate = new Date(`${new Date().getFullYear()}-${suppliedDateText.substring(suppliedDateText.length - 2, suppliedDateText.length)}-${suppliedDateText.substring(0, 2)}`)
+    displayDate.setHours(hour);
+    displayDate.setMinutes(minute);
+
+    return displayDate;
+ }
+
+ exports.createSessionRequestEmbed = function (partyMembers, displayDate, objective, message, bot) { 
+    let partyMembersText = createPartyMemberText(partyMembers);
+
+    let outputEmbed = new MessageEmbed()
+        .setColor(`#${decimalToHex(bot.sessions.nextSessionId)}`)
+        .setThumbnail(message.guild.iconURL())
+        .setTitle(`**Session_Request: **`)
+        .addFields(
+            { name: `**Session Commander:**`, value: `${partyMembers[0]}\n`, inline: false },
+            { name: `**Players(${partyMembers.length}/5):**`, value: `${partyMembersText}`, inline: false },
+            { name: `**DM:**`, value: `*TBD*`, inline: false },
+            { name: `**Time:**`, value: `*${days[displayDate.getDay()]} (${getDoubleDigitNumber(displayDate.getDate())}/${getDoubleDigitNumber(displayDate.getMonth() + 1)}) ${getDoubleDigitNumber(displayDate.getHours())}:${getDoubleDigitNumber(displayDate.getMinutes())}*`, inline: false },
+            { name: `**Location:**`, value: `*Roll20 (online)*`, inline: false },
+            { name: `**Objective:**`, value: `*${objective.trim()}*`, inline: false }
+        )
+        .setTimestamp()
+        .setFooter(`Requested by ${message.author.username}`, message.author.avatarURL());
+
+    return outputEmbed;
+}
+
+function createPartyMemberText(partyMembers){
+    let partyMembersText = 'Something went wrong!';
+
+    partyMembersText = `${partyMembers[0]}`;
+    for (let i = 1; i < partyMembers.length; i++) {
+        partyMembersText += `, ${partyMembers[i]}`;
+    }
+    return partyMembersText;
+}
