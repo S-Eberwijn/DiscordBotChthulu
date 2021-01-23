e6a662b6ede50921a9f5dc12a8064d977be64dd8
SEberwijn <11700946@student.pxl.be>
2021-01-23 18:26:11 +0000

    Integrated Repl.it

--- bot.js
+++ bot.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const { Client } = require('discord.js');
 const fs = require("fs");
 const Enmap = require('enmap');
+const keep_alive = require('./keep_alive.js')
+
 
 // Initialize Discord Bot
 const bot = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 bot.commands = new Enmap();
- 
+
 // Read and log command files
 fs.readdir("./commands/", async (err, dirs) => {
     if (err) console.log(err);
@@ -20,9 +22,9 @@ fs.readdir("./commands/", async (err, dirs) => {
             var jsFiles = files.filter(f => f.split(".").pop() === "js");
             if (dirs.length <= 0) {
                 console.log("Found no dirs files!");
-                return; 
+                return;
             }
-            jsFiles.forEach((f,i) => {
+            jsFiles.forEach((f, i) => {
                 var fileGet = require(`./commands/${d}/${f}`);
                 console.log(`--{ Command ${f} is loaded }--`);
                 var commandName = fileGet.help.name;
@@ -34,9 +36,9 @@ fs.readdir("./commands/", async (err, dirs) => {
 
 // Read and log event files
 fs.readdir('./events/', (err, files) => {
-    if(err) console.log(err);
+    if (err) console.log(err);
     files.forEach(file => {
-        if(!file.endsWith('.js')) return;
+        if (!file.endsWith('.js')) return;
         const evt = require(`./events/${file}`);
         let evtName = file.split('.')[0];
         console.log(`--{ Event ${evtName} is loaded }--`);
